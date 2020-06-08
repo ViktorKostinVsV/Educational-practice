dbf1ce1cd0321bc5b7ca58a84d6db942cfda6e3d
ViktorKostinVsV <63226527+ViktorKostinVsV@users.noreply.github.com>
2020-06-08 22:35:31 +0500

    Add files via upload

---
+++ practice№6/practice№6/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace practice_6
+{
+    class Program
+    {
+        static int a1;
+        static int a2;
+        static int a3;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите а1: ");
+            a1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите а2: ");
+            a2 = int.Parse(Console.ReadLine());
+            Console.Write("Введите а3: ");
+            a3 = int.Parse(Console.ReadLine());
+            Console.Write("Введите N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            int[] result = new int[N];
+
+            Console.Write("Итоговая последовательность: ");
+            for(int i = 1; i <= N; i++)
+            {
+                Console.Write(Function(i) + " ");
+            }
+
+            if (Function(2) > Function(4))
+            {
+                Console.WriteLine("Последовательность убывающая");
+            }
+            else if(Function(2) < Function(4))
+            {
+                Console.WriteLine("Последовательность возрастающая");
+            }
+            else
+            {
+                Console.WriteLine("Последовательность остается не изменной");
+            }
+
+            Console.WriteLine();
+        }
+        static int Function(int a)
+        {
+            int result;
+
+            if (a == 1)
+            {
+                return a1;
+            }
+            else if (a == 2)
+            {
+                return a2;
+            }
+            else if (a == 3)
+            {
+                return a3;
+            }
+            else
+            {
+                result = 13 * Function(a - 1) - 10 * Function(a - 2) + Function(a - 3);
+                return result;
+            }
+        }
+
+        static void Check(int[] array)
+        {
+            bool check = true;
+            for(int i = 3; i < array.Length; i = i + 2)
+            {
+                if (array[i] < array[i - 2])
+                {
+                    check = false;
+                    break;
+                }
+            }
+            Console.WriteLine("Четные элементы образуют возрастающую подпоследовательность: " + check);
+        }
+    }
+}
