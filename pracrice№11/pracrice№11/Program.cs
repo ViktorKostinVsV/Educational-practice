7c9ace08a1bb2d8fcab4a866834271202a200757
ViktorKostinVsV <63226527+ViktorKostinVsV@users.noreply.github.com>
2020-06-08 22:31:45 +0500

    Add files via upload

---
+++ pracrice№11/pracrice№11/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+
+namespace pracrice_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите текст: ");
+            StringBuilder builder = new StringBuilder(Console.ReadLine());
+
+            builder = Encrypt(builder, 1);
+
+            Console.WriteLine(builder);
+
+            builder = Decrypt(builder, 1);
+
+            Console.WriteLine(builder);
+
+        }
+
+        static StringBuilder Encrypt(StringBuilder builder, int n)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < builder.Length; i++)
+                {
+                    if (builder[i] >= (char)1040 && builder[i] < (char)1045 || (builder[i] >= (char)1046 && builder[i] < (char)1071)
+                        || (builder[i] >= (char)1072 && builder[i] < (char)1077 || (builder[i] >= (char)1078 && builder[i] < (char)1103)))
+                    {
+                        builder[i]++;
+                    }
+                    else if (builder[i] == (char)1071)
+                    {
+                        builder[i] = 'А';
+                    }
+                    else if (builder[i] == (char)1103)
+                    {
+                        builder[i] = 'а';
+                    }
+                    else if (builder[i] == (char)1045)
+                    {
+                        builder[i] = 'Ё';
+                    }
+                    else if (builder[i] == (char)1077)
+                    {
+                        builder[i] = 'ё';
+                    }
+                    else if (builder[i] == (char)1025)
+                    {
+                        builder[i] = 'Ж';
+                    }
+                    else if (builder[i] == (char)1105)
+                    {
+                        builder[i] = 'ж';
+                    }
+                }
+            }
+
+            return builder;
+        }
+
+        static StringBuilder Decrypt(StringBuilder builder, int n)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                for (int i = 0; i < builder.Length; i++)
+                {
+                    if (builder[i] > (char)1040 && builder[i] <= (char)1045 || (builder[i] > (char)1046 && builder[i] <= (char)1071)
+                        || (builder[i] > (char)1072 && builder[i] <= (char)1077 || (builder[i] > (char)1078 && builder[i] <= (char)1103)))
+                    {
+                        builder[i]--;
+                    }
+                    else if (builder[i] == (char)1040)
+                    {
+                        builder[i] = 'Я';
+                    }
+                    else if (builder[i] == (char)1072)
+                    {
+                        builder[i] = 'я';
+                    }
+                    else if (builder[i] == (char)1046)
+                    {
+                        builder[i] = 'Ё';
+                    }
+                    else if (builder[i] == (char)1078)
+                    {
+                        builder[i] = 'ё';
+                    }
+                    else if (builder[i] == (char)1025)
+                    {
+                        builder[i] = 'Е';
+                    }
+                    else if (builder[i] == (char)1105)
+                    {
+                        builder[i] = 'е';
+                    }
+                }
+            }
+
+            return builder;
+        }
+    }
+
+}
---
+++ pracrice№12/pracrice№12/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+
+namespace pracrice_12
+{
+    class Program
+    {
+        static int count = 0;
+        static int count2 = 0;
+
+        static int AddInPyramid(int[] array, int i, int N)
+        {
+            int imax;
+            int buf;
+            if ((2 * i + 2) < N)
+            {
+                count2++;
+                if (array[2 * i + 1] < array[2 * i + 2]) imax = 2 * i + 2;
+                else imax = 2 * i + 1;
+            }
+            else imax = 2 * i + 1;
+            if (imax >= N)
+            {
+                count2++;
+                return i;
+            }
+            if (array[i] < array[imax])
+            {
+                count2++;
+                count++;
+                buf = array[i];
+                array[i] = array[imax];
+                array[imax] = buf;
+                if (imax < N / 2) i = imax;
+            }
+            return i;
+        }
+
+        static void Pyramid(int[] array, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; --i)
+            {
+                long prev_i = i;
+                i = AddInPyramid(array, i, n);
+                if (prev_i != i)
+                {
+                    count2++;
+                    ++i;
+                }
+            }
+
+            int buf;
+            for (int k = n - 1; k > 0; --k)
+            {
+                count++;
+                buf = array[0];
+                array[0] = array[k];
+                array[k] = buf;
+                int i = 0, prev_i = -1;
+                while (i != prev_i)
+                {
+                    count2++;
+                    prev_i = i;
+                    i = AddInPyramid(array, i, k);
+                }
+            }
+        }
+
+        static void CountingSort(int[] array)
+        {
+            var min = array[0];
+            var max = array[0];
+            foreach (int element in array)
+            {
+                
+                if (element > max)
+                {
+                    count2++;
+                    max = element;
+                }
+                else if (element < min)
+                {
+                    count2++;
+                    min = element;
+                }
+            }
+
+            count2++;
+            var correctionFactor = min != 0 ? -min : 0;
+            max += correctionFactor;
+
+            int[] count = new int[max + 1];
+            for (var i = 0; i < array.Length; i++)
+            {
+                count[array[i] + correctionFactor]++;
+            }
+
+            var index = 0;
+            for (var i = 0; i < count.Length; i++)
+            {
+                for (var j = 0; j < count[i]; j++)
+                {
+                    Program.count++;
+                    array[index] = i - correctionFactor;
+                    index++;
+                }
+            }
+        }
+
+        static int[] MakeArray(int c)
+        {
+            Random rnd = new Random();
+            int[] mas = new int[c];
+
+            for(int i = 0; i < c; i++)
+            {
+                mas[i] = rnd.Next(1, 100);
+            }
+
+            return mas;
+        }
+
+        static int[] MakeSortedArray(int c)
+        { 
+            int[] mas = new int[c];
+
+            for (int i = 0; i < c; i++)
+            {
+                mas[i] = i;
+            }
+
+            return mas;
+        }
+
+        static int[] MakeUnsortedArray(int c)
+        {
+            int[] mas = new int[c];
+
+            for (int i = 0; i < c; i++)
+            {
+                mas[i] = c-i-1;
+            }
+
+            return mas;
+        }
+
+        static void ShowArray(int[] array)
+        {
+            foreach(int i in array)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = MakeArray(20);
+            ShowArray(array);
+            Pyramid(array, array.Length);
+            Console.WriteLine();
+            Console.WriteLine("Кол-во перемещений: "+ count);
+            Console.WriteLine("Кол-во сравнений: "+count2);
+            count = 0;
+            count2 = 0;
+            Console.WriteLine();
+            ShowArray(array);
+
+            array = MakeArray(20);
+            ShowArray(array);
+            CountingSort(array);
+            Console.WriteLine();
+            Console.WriteLine("Кол-во перемещений: " + count);
+            Console.WriteLine("Кол-во сравнений: " + count2);
+            count = 0;
+            count2 = 0;
+            Console.WriteLine();
+            ShowArray(array);
+
+            array = MakeSortedArray(20);
+            ShowArray(array);
+            Pyramid(array, array.Length);
+            Console.WriteLine();
+            Console.WriteLine("Кол-во перемещений: " + count);
+            Console.WriteLine("Кол-во сравнений: " + count2);
+            count = 0;
+            count2 = 0;
+            Console.WriteLine();
+            ShowArray(array);
+
+            ShowArray(array);
+            CountingSort(array);
+            Console.WriteLine();
+            Console.WriteLine("Кол-во перемещений: " + count);
+            Console.WriteLine("Кол-во сравнений: " + count2);
+            count = 0;
+            count2 = 0;
+            Console.WriteLine();
+            ShowArray(array);
+
+            array = MakeUnsortedArray(20);
+            ShowArray(array);
+            Pyramid(array, array.Length);
+            Console.WriteLine();
+            Console.WriteLine("Кол-во перемещений: " + count);
+            Console.WriteLine("Кол-во сравнений: " + count2);
+            count = 0;
+            count2 = 0;
+            Console.WriteLine();
+            ShowArray(array);
+
+            array = MakeUnsortedArray(20);
+            ShowArray(array);
+            CountingSort(array);
+            Console.WriteLine();
+            Console.WriteLine("Кол-во перемещений: " + count);
+            Console.WriteLine("Кол-во сравнений: " + count2);
+            count = 0;
+            count2 = 0;
+            Console.WriteLine();
+            ShowArray(array);
+
+        }
+    }
+}
