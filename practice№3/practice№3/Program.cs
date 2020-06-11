ad22972acac8be479eceaee07b717375204a0802
ViktorKostinVsV <63226527+ViktorKostinVsV@users.noreply.github.com>
2020-06-11 21:12:50 +0500

    Add files via upload

--- practice№3/practice№3/Program.cs
+++ practice№3/practice№3/Program.cs
@@ -6,9 +6,14 @@ namespace practice_3
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите x: ");
             double userX = double.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
             double userY = double.Parse(Console.ReadLine());
 
+            Console.Write("u = ");
+
+            // Проверка на совпадение с фигурой.
             if (userY >= 0 &&
                 Math.Abs(Math.Pow(userX, 2) + Math.Pow(userY, 2)) <= 1 &&
                 (Math.Abs(Math.Pow(userX, 2) + Math.Pow(userY, 2)) >= Math.Pow(0.3, 2) || (userX <= 0)))
