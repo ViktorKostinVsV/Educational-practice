5e958a0b8b8b80f09ab73af4ea6585fb73a248c7
ViktorKostinVsV <63226527+ViktorKostinVsV@users.noreply.github.com>
2020-06-11 21:13:33 +0500

    Add files via upload

--- practice№5/practice№5/Program.cs
+++ practice№5/practice№5/Program.cs
@@ -7,6 +7,7 @@ namespace practice_5
 
         static Random rnd = new Random();
 
+        // Создаем массив.
         static int[,] MakeArray()
         {
             int[,] arr = new int[9, 9];
@@ -26,6 +27,7 @@ namespace practice_5
             return arr;
         }
 
+        // Изменяем массив в соответствии с заданием.
         static int[,] NewArray(int[,] arr)
         {
             int equal;
@@ -38,6 +40,7 @@ namespace practice_5
                 {
                     if (i != j)
                     {
+                        // Если элемент больше элемента главной дигонали, заменяется на 1, иначе - на 0.
                         if (equal < arr[i, j])
                         {
                             arr[i, j] = 1;
@@ -46,25 +49,38 @@ namespace practice_5
                         {
                             arr[i, j] = 0;
                         }
-                        Console.Write(arr[i, j] + " ");
                     }
                     else
                     {
-                        Console.Write(arr[i, j] + " ");
                     }
                 }
 
-                Console.WriteLine();
             }
 
             return arr;
         }
 
+        // Вывод массива.
+        static void Show(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                        Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Генерация массива...");
             int[,] arr = MakeArray();
-            Console.WriteLine();
+            Console.WriteLine("Измененный массив:");
             arr = NewArray(arr);
+            Show(arr);
         }
     }
 }
