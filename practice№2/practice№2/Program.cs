80a709cdfd0f8f283a1bbd8383167649037cb832
ViktorKostinVsV <63226527+ViktorKostinVsV@users.noreply.github.com>
2020-06-11 21:12:20 +0500

    Add files via upload

--- practice№2/practice№2/Program.cs
+++ practice№2/practice№2/Program.cs
@@ -6,34 +6,39 @@ namespace practice_2
     {
         public static int Main()
         {
+            // Ввод чисел.
             string[] masNumbers = Console.ReadLine().Split(' ');
 
-            int counter;
-            int sum;
-            string result = "";
+            int counter;        // Счетчик множителей.
+            int sum;            // Сумма чисел.
+            string result = ""; // Переменная для результата.
 
+            // Цикл для разложения чисел на простые множители.
             for (int k = 0; k < masNumbers.Length; k++)
             {
-                sum = 0;
-                counter = 0;
+                sum = 0;                            // Счетчик множителей.
+                counter = 0;                        // Сумма чисел.
+                int num = int.Parse(masNumbers[k]); // Число на разложение.
 
-                int num = int.Parse(masNumbers[k]);
-
+                // Раскладываем на множитель (2)
                 for (; num % 2 == 0; num /= 2)
                 {
                     sum += 2;
                     counter++;
                 }
 
+                // Раскладываем на све остальные множители.
                 for (int i = 3; i <= num;)
                 {
                     if (num % i == 0)
                     {
+                        // Если число простое и больше 9, считаем сумму чисел.
                         if (i > 9)
                         {
                             sum += Summ(i);
                             counter++;
                         }
+                        // Иначе добавляем число к сумме.
                         else if (i > 1)
                         {
                             sum += i;
@@ -47,6 +52,7 @@ namespace practice_2
                     }
                 }
 
+                // Если это число Смита, в результат пишется 1, иначе 0.
                 if (sum == Summ(int.Parse(masNumbers[k]))&& counter!=1)
                 {
 
@@ -62,6 +68,7 @@ namespace practice_2
             return 0;
         }
 
+        // Метод считающий числа.
         static public int Summ(int number)
         {
             int s = 0;
