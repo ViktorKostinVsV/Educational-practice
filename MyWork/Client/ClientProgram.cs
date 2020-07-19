8994285925171eebd541cce828927d56be4f42e6
ViktorKostinVsV <63226527+ViktorKostinVsV@users.noreply.github.com>
2020-07-19 16:26:07 +0500

    Add files via upload

---
+++ MyWork/Client/ClientProgram.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        const int port = 8888;              //   Порт сервера.
+        const string address = "127.0.0.1"; //   Адресс сервера.
+
+
+        static TcpClient client;            //   Клиентское подключение по TCP.
+        static NetworkStream stream;        //   Поток данных для доступа к сети.
+        static StreamWriter writer;         //   Переменная для записи в поток.
+        static StreamReader reader;         //   Переменная для считывания в поток.
+
+
+        static FileStream fileStream = new FileStream("Result.txt", FileMode.Create, FileAccess.Write); //   Создание файла для сохранения.
+        static StreamWriter streamWriter = new StreamWriter(fileStream);                                //   Поток для записи в файл.
+
+
+        //   Соединяемся с сервером.
+        static void Connect()
+        {
+            client = new TcpClient(address, port);
+            stream = client.GetStream();
+        }
+
+        //   Получаем файлы на обработку.
+        static string[] GetFiles()
+        {
+            string[] files = null;
+
+            do
+            {
+                while(true)
+                {
+                    try
+                    {
+                        Console.Write("Введите полный путь к папке с файлами: ");
+                        var dir = @"" + Console.ReadLine();
+
+                        files = Directory.EnumerateFiles(dir, "*.txt")
+                            .Select(File.ReadAllText).ToArray();
+
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                }
+            } while (files.Length == 0 || files.Length > 127 || files == null);
+
+            return files;
+        }
+
+        //   Отправляем запрос.
+        static void StreamWriter(string files)
+        {
+            
+            string s = Regex.Replace(files, @"[ \r\n\t]", "");
+            s.ToLower();
+
+            writer.WriteLine(s.ToCharArray(),0,s.Length);
+            writer.Flush();
+
+            Thread.Sleep(1000);
+            
+        }
+
+        //   Получаем ответ.
+        static void StreamReader()
+        {
+            while (true)
+            {
+                try
+                {
+                    reader = new StreamReader(stream);
+
+                    string message = reader.ReadLine();
+                    Console.WriteLine(message);
+
+                    streamWriter.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!"); //соединение было прервано
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        //   Отключаем соединение с сервером.
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();
+            if (client != null)
+                client.Close();
+
+            Environment.Exit(0); 
+        }
+
+        //   Создание потока для получение ответа.
+        static void NewThread()
+        {
+            Thread readThread = new Thread(new ThreadStart(StreamReader));
+            readThread.Start(); //старт потока
+        }
+
+        //   Закрытие открытых полтоков.
+        static void ThreadClose()
+        {
+            Console.WriteLine("Фаил сохранен в: " + fileStream.Name);
+
+            streamWriter.Close();
+            fileStream.Close();
+
+        }
+
+        //   Главный метод
+        static void MainProcess()
+        {
+            TcpClient client = null;
+
+            try
+            {
+                Connect();
+                NewThread();
+
+                string[] files = GetFiles();
+             
+                for (int i = 0; i < files.Length; i++)
+                {
+                    writer = new StreamWriter(stream);
+                    StreamWriter(files[i]);
+                }
+
+                Thread.Sleep(5000);
+
+                ThreadClose();
+
+                Console.WriteLine("Нажмите любую кнопку для выхода...");
+                Console.ReadLine();
+                Disconnect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+
+        }
+
+        //   Настоящий главный метод.
+        static void Main(string[] args)
+        {
+            MainProcess();
+        }
+    }
+}
---
+++ MyWork/Server/ServerProgram.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+
+namespace ConsoleServer
+{
+    class Program
+    {
+        const int port = 8888;              //   Порт для подключения.
+
+
+        static TcpListener listener;        //   Прослушивание подключениев TCP.
+        static public TcpClient client;     //   Подключенный TCP клиент.
+        static public NetworkStream stream; //   Поток данных для доступа к сети.
+
+
+        static int maxThread;               //   Максимальное кол-во потоков.
+        static int indexFile = 1;           //   Нумерация файла.
+
+
+        //   Запускаем сервер.
+        static void StartServer()
+        {
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+            listener.Start();
+        }
+
+        //   Останавливаем сервер.
+        static void StopServer()
+        {
+            if (listener != null)
+                listener.Stop();
+        }
+
+        //   Вводим ограничение по потокам.
+        static int InputUser()
+        {
+            bool check;
+            int number;
+
+            Console.WriteLine("Введите кол-во потоков (от 1 до 10): ");
+
+            do
+            {
+                check = int.TryParse(Console.ReadLine(), out number);
+
+                if (!check || number < 1 || number > 10)
+                {
+                    Console.Write("Неверное значение!" +
+                        "\nПовторите попытку: ");
+                }
+
+            } while (!check || number < 1 || number > 10);
+
+            return number;
+        }
+
+        //   Считываем поток.
+        static string StreamReader()
+        {            
+            try
+            {
+                StreamReader reader = new StreamReader(stream);
+                string message = reader.ReadLine();
+                Thread.Sleep(1000);
+
+                return message;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        //   Записываем поток.
+        static void StreamWriter(string message)
+        {           
+            try
+            {
+                StreamWriter writer = new StreamWriter(stream);
+                writer.WriteLine(message.ToCharArray(), 0, message.Length);
+                writer.Flush();
+            }
+            catch
+            {
+            }
+        }
+
+        //   Получаем значение палиндрома.
+        public static string Palindromtest(string s)
+        {
+            if (s != null)
+            {
+                for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+                {
+                    if (s[i] != s[j])
+                        return "false";
+                }
+                return "true";
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        //   Процесс передоваемый пулу.
+        static void Process(object o)
+        {
+            string result = (string)o;
+
+            if (result != null)
+            {
+                int i = indexFile;  //   Индексируем файл.
+                indexFile++;        //   Добавляем к индексу единицу.
+
+                result = Palindromtest(result);
+
+                Thread.Sleep(1000);
+
+                StreamWriter("Номер файла: "+i+" : "+result);
+            }
+            else if(client!=null)
+            {
+                indexFile = 1;
+                client = null;
+                Disconnect();
+            }
+        }
+
+        //   Добавляем в пул новый поток
+        static void NewThread(string result)
+        {
+            ThreadPool.QueueUserWorkItem(Process, result);      //   Добавляем метод Process в пулл.
+            ThreadPool.SetMaxThreads(maxThread, maxThread);     //   Устанавливаем максимальное кол-во потоков.
+            ThreadPool.SetMinThreads(1, 1);                     //   Устанавливаем минимльное кол-во потоков.
+        }
+
+        //   Ожидаем клиента.
+        static void ConnectListner()
+        {
+            Console.WriteLine("Ожидание клиента...");
+
+            client = listener.AcceptTcpClient();
+            stream = client.GetStream();
+
+            Console.WriteLine("Подключение.");
+        }
+
+        //   Отключаем клиента.
+        static void Disconnect()
+        {
+            try
+            {
+                stream.Close();          
+                client.Close();                
+            }
+            catch
+            {
+
+            }
+            client = null;
+            Console.WriteLine("Клиент был отключен.");
+        }
+
+        //   Главный метод.
+        static void MainProcess()
+        {
+            maxThread = InputUser();
+           
+            try
+            {
+                StartServer();
+
+                int worker = 1;
+                int io;
+
+                while (true)
+                {
+                    if (client == null)
+                    {
+                        worker = 1;
+                        ConnectListner();
+                    }
+
+                    try
+                    {
+                        string result = StreamReader();
+
+                        if (worker != 0) 
+                        {
+                            NewThread(result);
+                        }
+                        else
+                        {
+                            indexFile++;
+                            StreamWriter("Сервер перегружен...");
+                        }
+                        ThreadPool.GetAvailableThreads(out worker, out io);
+                    }
+                    catch 
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                StopServer();
+            }
+        }
+
+        //   Настоящий главный метод.
+        static void Main(string[] args)
+        {
+            MainProcess();
+        }
+    }
+}
